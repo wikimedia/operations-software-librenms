c0aa3ca458d62176bc56c0dbfeb5edb71e4cd53e
Alexandros Kosiaris <akosiaris@wikimedia.org>
2017-05-05 16:36:54 +0300

    Add WMF specific pages into the repo
    Add 2 librenms pages that we have created in the repo. Those are
    aggregates for interfaces per interface description and notes
    
    Change-Id: I002b05438d3a72e5bb206d96df4261275d452e6a

---
+++ html/pages/ifdescr.inc.php
@@ -0,0 +1,86 @@
+<table class="table table-condensed table-hover table-striped">
+    <tr bgcolor='$iftype'>
+        <th>Device</th>
+        <th>Interface</th>
+        <th>Speed</th>
+        <th>Circuit</th>
+        <th>Notes</th>
+    </tr>
+
+<?php
+
+if ($bg == '#ffffff') {
+    $bg = '#e5e5e5';
+}
+else {
+    $bg = '#ffffff';
+}
+
+$type_where = 'port_descr_descr = ?';
+$type_param[] = $vars['descr'];
+
+$ports       = dbFetchRows("SELECT * FROM `ports` as I, `devices` AS D WHERE $type_where AND I.device_id = D.device_id ORDER BY I.ifAlias", $type_param);
+
+foreach ($ports as $port) {
+    $if_list  .= $seperator.$port['port_id'];
+    $seperator = ',';
+}
+
+unset($seperator);
+
+echo "<tr class='iftype'>
+    <td colspan='5'><span class=list-large>Total Graph for ports with description: ".$vars['descr'].'</span><br />';
+
+if ($if_list) {
+    $graph_type      = 'multiport_bits_separate';
+    $port['port_id'] = $if_list;
+
+    include 'includes/print-interface-graphs.inc.php';
+
+    echo '</td></tr>';
+
+    foreach ($ports as $port) {
+        $done = 'yes';
+        unset($class);
+        $port['ifAlias'] = str_ireplace($type.': ', '', $port['ifAlias']);
+        $port['ifAlias'] = str_ireplace('[PNI]', 'Private', $port['ifAlias']);
+        $ifclass         = ifclass($port['ifOperStatus'], $port['ifAdminStatus']);
+        if ($bg == '#ffffff') {
+            $bg = '#e5e5e5';
+        }
+        else {
+            $bg = '#ffffff';
+        }
+
+        echo "<tr class='iftype'>
+            <td><span class=list-large>".generate_port_link($port, $port['port_descr_descr'])."</span><br />
+            <span class=interface-desc style='float: left;'>".generate_device_link($port).' '.generate_port_link($port).' </span></td>
+            <td>'.generate_port_link($port, makeshortif($port['ifDescr'])).'</td>
+            <td>'.$port['port_descr_speed'].'</td>
+            <td>'.$port['port_descr_circuit'].'</td>
+            <td>'.$port['port_descr_notes']."</td>
+            </tr>
+            <tr class='iftype'>
+            <td colspan='5'";
+
+        if (dbFetchCell('SELECT count(*) FROM mac_accounting WHERE port_id = ?', array($port['port_id']))) {
+            echo "<span style='float: right;'><a href='" . generate_url(array('page'=>'device', 'device'=>$port['device_id'], 'tab'=>'port', 'port'=>$port['port_id'], 'view'=>'macaccounting')) . "'><img src='images/16/chart_curve.png' align='absmiddle'> MAC Accounting</a></span>";
+        }
+
+        echo '<br />';
+
+        if (file_exists($config['rrd_dir'].'/'.$port['hostname'].'/port-'.$port['ifIndex'].'.rrd')) {
+            $graph_type = 'port_bits';
+
+            include 'includes/print-interface-graphs.inc.php';
+        }
+
+        echo '</td></tr>';
+    }
+}
+else {
+    echo 'None found.</td></tr>';
+}
+
+?>
+</table>
