79f63b6a5a4ca8762d0360668d48b512858e262a
Arzhel Younsi <ayounsi@wikimedia.org>
2019-09-11 10:39:55 -0700

    Add composer requirements for LibreNMS 1.55

---
+++ vendor/amenadiel/jpgraph/src/graph/Rectangle.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * JPGraph v3.6.21
+ */
+class Rectangle
+{
+    public $x;
+    public $y;
+    public $w;
+    public $h;
+    public $xe;
+    public $ye;
+
+    public function __construct($aX, $aY, $aWidth, $aHeight)
+    {
+        $this->x  = $aX;
+        $this->y  = $aY;
+        $this->w  = $aWidth;
+        $this->h  = $aHeight;
+        $this->xe = $aX + $aWidth - 1;
+        $this->ye = $aY + $aHeight - 1;
+    }
+}
