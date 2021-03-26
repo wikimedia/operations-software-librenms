e7727e3e3262f230245c9faa39c6bf9973dd8c13
Filippo Giunchedi <filippo@debian.org>
2021-03-26 09:17:09 +0100

    Use 'title' as Alertmanager summary
    The formatted title contains more information (hostname):
    
    summary = Primary outbound port utilisation over 80% #page
    title = Alert for device cr1-codfw.wikimedia.org - Primary outbound port utilisation over 80% #page
    
    Thus prefer it as 'summary' (the "standard" annotation) as requested in
    T273716
    
    Bug: T273716
    Change-Id: I12ac21d877c3bb26b0c331237499364e4eb3244a

--- LibreNMS/Alert/Transport/Alertmanager.php
+++ LibreNMS/Alert/Transport/Alertmanager.php
@@ -51,8 +51,7 @@ class Alertmanager extends Transport
             $alertmanager_status => date("c"),
             'generatorURL' => $gen_url,
             'annotations' => [
-                'summary' => $obj['name'],
-                'title' => $obj['title'],
+                'summary' => $obj['title'],
                 'description' => $alertmanager_msg,
             ],
             'labels' => [
