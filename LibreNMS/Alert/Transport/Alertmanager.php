0d583b27bb840372186e98ea1031103c1e8b5975
Filippo Giunchedi <filippo@debian.org>
2021-03-26 10:36:30 +0100

    Use device's 'alerts' page as Alertmanager 'source' link
    Bug: T273716
    Change-Id: I955cbfc2441244902d19a242349863844dd83150

--- LibreNMS/Alert/Transport/Alertmanager.php
+++ LibreNMS/Alert/Transport/Alertmanager.php
@@ -40,7 +40,6 @@ class Alertmanager extends Transport
         $url = $this->config['alertmanager-url'];
         $username = $this->config['alertmanager-username'];
         $password = $this->config['alertmanager-password'];
-
         $alertmanager_status = $alert_data['state'] == AlertState::RECOVERED ? 'endsAt' : 'startsAt';
         $alertmanager_msg = strip_tags((string) $alert_data['msg']);
         $data = [[
