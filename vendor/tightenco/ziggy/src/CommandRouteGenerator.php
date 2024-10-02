b1ea256462234899577ad7f8dbfc1cf9ccb741bf
Andrea Denisse Gómez-Martínez <adenisse@wikimedia.org>
2024-10-02 11:58:01 -0600

    Add composer requirements for LibreNMS 24.9.1
    Change-Id: Ie8d481ce0d37a2c7637e1f50c49c75712048057f

---
+++ vendor/tightenco/ziggy/src/CommandRouteGenerator.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Tightenco\Ziggy;
+
+use Illuminate\Console\Command;
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Str;
+use Tightenco\Ziggy\Output\File;
+use Tightenco\Ziggy\Output\Types;
+use Tightenco\Ziggy\Ziggy;
+
+class CommandRouteGenerator extends Command
+{
+    protected $signature = 'ziggy:generate
+                            {path? : Path to the generated JavaScript file. Default: `resources/js/ziggy.js`.}
+                            {--types : Generate a TypeScript declaration file.}
+                            {--types-only : Generate only a TypeScript declaration file.}
+                            {--url=}
+                            {--group=}';
+
+    protected $description = 'Generate a JavaScript file containing Ziggy’s routes and configuration.';
+
+    protected $files;
+
+    public function __construct(Filesystem $files)
+    {
+        parent::__construct();
+
+        $this->files = $files;
+    }
+
+    public function handle()
+    {
+        $ziggy = new Ziggy($this->option('group'), $this->option('url') ? url($this->option('url')) : null);
+
+        $path = $this->argument('path') ?? config('ziggy.output.path', 'resources/js/ziggy.js');
+
+        if ($this->files->isDirectory(base_path($path))) {
+            $path .= '/ziggy';
+        } else {
+            $this->makeDirectory($path);
+        }
+
+        $name = preg_replace('/(\.d)?\.ts$|\.js$/', '', $path);
+
+        if (! $this->option('types-only')) {
+            $output = config('ziggy.output.file', File::class);
+
+            $this->files->put(base_path("{$name}.js"), new $output($ziggy));
+        }
+
+        if ($this->option('types') || $this->option('types-only')) {
+            $types = config('ziggy.output.types', Types::class);
+
+            $this->files->put(base_path("{$name}.d.ts"), new $types($ziggy));
+        }
+
+        $this->info('Files generated!');
+    }
+
+    protected function makeDirectory($path)
+    {
+        if (! $this->files->isDirectory(dirname(base_path($path)))) {
+            $this->files->makeDirectory(dirname(base_path($path)), 0755, true, true);
+        }
+
+        return $path;
+    }
+}
